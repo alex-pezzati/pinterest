8f995db74629c338042fc43f285f1424c2352dd2
alex-pezzati <aapezzati@gmail.com>
2021-02-18 17:09:16 -0500

    user logout routes setup and testing complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,4 +31,13 @@ router.post(
     }),
 );
 
+// logout route
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+        }
+);
+
 module.exports = router;
