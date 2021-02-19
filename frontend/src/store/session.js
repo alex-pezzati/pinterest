4b7c45fd699540add81875379064b157c5ff39fe
alex-pezzati <aapezzati@gmail.com>
2021-02-19 00:12:32 -0500

    login form functionality setup and tested

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,7 @@ const removeUser = () => {
     };
 };
 
+// login user to state
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
@@ -30,6 +31,16 @@ export const login = (user) => async (dispatch) => {
     dispatch(setUser(data.user));
     return response;
 };
+
+// restores user to state
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+
 
 const initialState = { user: null };
 
