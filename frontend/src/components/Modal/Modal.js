e75184238b421ffebbf21e708bfffa9ec1e51b79
alex-pezzati <aapezzati@gmail.com>
2021-02-23 17:13:57 -0500

    initial modal progress

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
+
+Modal.setAppElement('#root');
+
+function App() {
+    var subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return (
+        <div>
+            <button onClick={openModal}>Open Modal</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <h2 red={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+                <button onClick={closeModal}>close</button>
+                <div>I am a modal</div>
+                <form>
+                    <input />
+                    <button>tab navigation</button>
+                    <button>stays</button>
+                    <button>inside</button>
+                    <button>the modal</button>
+                </ form>
+            </Modal>
+        </div>
+    )
+}
+
+ReactDom.render(<App />, appElement)
